3ca242230a8013e788ee5e2adf00108e2edf77fe
Tanookerz <125132323+Tanookerz@users.noreply.github.com>
2023-03-05 13:17:04 +0800

    A program to test the use of lettered multiple choices instead of numbers

---
+++ multi_choice_test.py
@@ -0,0 +1,11 @@
+import time
+print('Filipino spaghetti tastes like utter fucking shit')
+time.sleep(1.0)
+print('Do you agree with this statement?')
+opinion = input('a. Yes b. No c. I don\'t know ')
+if opinion == "A" or opinion == "a":
+    print("shit opinion, go die")
+elif opinion == "B" or opinion == "b":
+    print("you have good taste, glad you picked the right choice.")
+elif opinion == "C" or opinion == "c":
+    print("Indecisive little bitch, go back and answer the damn question again and choose an actual answer.")
